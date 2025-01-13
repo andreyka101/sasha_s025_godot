6576c9fb2afd35ff5a48380081c50cc7e1705b72
andreyka101 <andrejkvasnin236@gmail.com>
2025-01-13 19:36:21 +0300

    efeffe

--- button_2.gd
+++ button_2.gd
@@ -1,21 +1,41 @@
+
+# получаем главный узел сцены
 extends Button
 
 
 
-
+# нажатие кнопки присоединил через узел
 func _on_pressed() -> void:
-	position.y += 20
+
+	# меняем координаты
+	self.position.y += 20
 	position.x = 10
+
+	# меняем размер
 	size.x = 500
 	print(size.x)
 	
+	# меняем точку вращения
 	pivot_offset.x = size.x / 2
 	pivot_offset.y = size.y / 2
 
+	# крутим
 	rotation += 1
 	
+	# меняем текст кнопки
 	#text = "hello"
-	#text = position.y
+	text = str(position.y)
 	
 	#for i in range(10):
 		#print(i)
+	
+	# два способа изминить цвет текста
+	set("theme_override_colors/font_color" , "#00FFFF")
+	add_theme_color_override("font_focus_color" ,"#8B00FF")
+
+	
+	var new_style = StyleBoxFlat.new()
+	new_style.bg_color = "#FF7518"
+	set("theme_override_styles/normal" , new_style)
+	
+	
--- level_1_2d.gd
+++ level_1_2d.gd
@@ -2,16 +2,24 @@ extends Node2D
 
 
 
+# создание обычной переменной
+var num = 5 
 
-var num = 5
 
 
 
+# нажатие кнопки присоединил через узел
 func _on_button_pressed() -> void:
 	print("ok")
 	#var num = 5
-	print(num)
-	num = "2e"
-	#num += 1
+	#print(num)
+	#num = "2"
+	num += 1
+	
+	
+	#var but_2 = get_node("Button2")
+	var but_2:BaseButton = get_node("Button2")
+	but_2.position.x = 800
+	but_2.position.y = 50
 	
 	
